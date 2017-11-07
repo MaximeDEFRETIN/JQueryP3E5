dddf8d8d86c08e26aced0bc78f816b3817d4cd0c
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-11-07 10:20:34 +0100

    Correction

--- javascript.js
+++ javascript.js
@@ -1,59 +1,50 @@
-$('#element').css('width','50px').css('height','50px').css('background','red');
-var x = 0;
-var y = 0;
-
-//keyup() -> execute la fonction lorsqu'une touche a été pressé puis relaché
-$('#texte').keyup(function(e) {
-
-  //e.keyCode -> renvoie le numéro de la touche préssée
-  var key = e.keyCode;
-  //window -> correspond à la fenêtre du navigateur.
-  //Si window est mis dans des simples cotes, c'est alors considéré comme un problème et le script s'arrête.
-  var width = $(window).width();
-  var height = $(window).height();
-
-  if (key == 37) {
-    y = y - 10;
-    //On donne les instructions en fonction de la position du carré dans l'écran
-    if(y < 0){
-      y = width - 50;
-      $('#element').css('margin-left', y);
-    }
-    else if (y >= 0) {
-      y = y - 10;
-      $('#element').css('margin-left', y);
-    }
-  }
-  else if (key == 38) {
-    x = x - 10;
-    if (x < 0) {
-      //-90 correspond à la taille du carré
-      x = height - 90;
-      $('#element').css('margin-top', x);
-    }
-    else if (x >= 0) {
-      x = x - 10;
-      $('#element').css('margin-top', x);
-    }
-  }
-  else if (key == 39) {
-    y = + 10;
-    if (width - 50 > y) {
-      $('#element').css('margin-top', y);
-    }
-    else if (width - 50 <= y) {
-      y = 0;
-      $('#carre').css('margin-left', y);
-    }
-  }
-  else if (key == 40) {
-    x = x + 10;
-    if (width - 50 > x) {
-      $('#element').css('margin-top', x);
-    }
-    else if (width - 50 <= x) {
-      x = 0;
-      $('#element').css('margin-left', x);
-    }
-  }
-});
+$(function(){
+      //On déterminer la largeur de la fenêtre
+      var widthWindow = $(window).width();
+      //On détermine la hauteur de la fenêtre
+      var heightWindow = $(window).height();
+      //Losqu'on appuie sur une touche
+      $('#text').keydown(function(e){
+        //On détermine la postion horizontal de la div
+        var positionSquareX = $('#square').offset().left;
+        //On détermine la positon vertical de la div
+        var positionSquareY = $('#square').offset().top;
+//e.keyCode = valeur d'une touche de clavier
+        switch (e.keyCode) {
+          case 39://touche droite
+          //Si la positon horizontal et plus grand ou égale a la largeur de l'écran
+          if(positionSquareX >= (widthWindow-105)){
+            //Le carrer a un margin a 0
+            $('#square').css('margin-left', '0');
+          }else {
+            // Sinon le carre gagne 10px en margin left
+            $('#square').css('margin-left', '+=10px');
+          }
+          break;
+          case 37://touche gauche
+          if(positionSquareX <= 0){
+            $('#square').css('margin-left', widthWindow-105);
+          }else {
+            $('#square').css('margin-left', '-=10px');
+          }
+          break;
+          case 38://touche haut
+          //Si la postion Verticale est inférieur ou égale a 0
+          if(positionSquareY <= 0){
+            //Le carrer auras un margin-top équibalent a la hauteur-125
+            $('#square').css('margin-top', heightWindow-125);
+            //Sinon margin-top auras +10
+          }else {
+            $('#square').css('margin-top', '-=10px');
+          }
+          break;
+          case 40://touche bas
+          if(positionSquareY >= (heightWindow-105)){
+            $('#square').css('margin-top', '0');
+          }else {
+            $('#square').css('margin-top', '+=10px');
+          }
+          break;
+        }
+      });
+    });
